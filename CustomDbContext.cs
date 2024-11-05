e07c168eb7aaecbff5b552a54d389f2bbe0ae1f4
Shahad <Shahadalnaamani@outlook.com>
2024-11-05 14:36:55 +0400

    Created Dbcontext class and model classes

---
+++ CustomDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORMCodeFirst
+{
+    public class CustomDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(" Data Source=(local); Initial Catalog=ORMCodeFirst; Integrated Security=true; TrustServerCertificate=True ");
+        }
+    }
+}
--- obj/Debug/net8.0/ORMCodeFirst.AssemblyInfo.cs
+++ obj/Debug/net8.0/ORMCodeFirst.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ORMCodeFirst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6113ff2b433a8b9d308250d17cca9bf9dd340185")]
 [assembly: System.Reflection.AssemblyProductAttribute("ORMCodeFirst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ORMCodeFirst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
